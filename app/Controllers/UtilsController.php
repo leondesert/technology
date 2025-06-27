81b88a837735ce3b24cc8352689bfac328e7fc37
leondesert <wkenesary@gmail.com>
2025-06-27 22:20:52 +0500

    success project

--- app/Controllers/UtilsController.php
+++ app/Controllers/UtilsController.php
@@ -5,10 +5,11 @@ namespace App\Controllers;
 class UtilsController extends BaseController
 {
 
-    // функция для округления в большую сторону
     public static function rounding($number)
     {   
-        return round($number, 2);
+        // Округляем число до 2 знаков после запятой и форматируем его как строку
+        // с двумя знаками после запятой, используя точку в качестве десятичного разделителя.
+        return number_format((float)round($number, 2), 2, '.', '');
     }
 
 }
